c5bd532be7f121b049ff701281f3ffad84203663
mansidw <mansi.dwivedi@spit.ac.in>
2022-05-04 00:20:57 +0530

    db conn

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 from datetime import timedelta
 import django_heroku
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 import environ
@@ -65,8 +66,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
 }
 
